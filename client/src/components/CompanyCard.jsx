f5ea99e0d0db34e95c8034964ec979e1307be7c9
luageo <ldeoliveiratech@gmail.com>
2024-07-04 23:40:04 +0200

    css companyCard

--- client/src/components/CompanyCard.jsx
+++ client/src/components/CompanyCard.jsx
@@ -6,18 +6,22 @@ function CompanyCard({ company }) {
   return (
     <div
       className="company-card"
-      style={{ backgroundImage: `url(${companyCardBG})` }}
+      style={{
+        backgroundImage: `url(${companyCardBG})`,
+        width: "300px",
+        height: "400px",
+      }}
     >
       <div className="company-card-header">
+        <div className="company-card-body">
+          <h3>{company.raisonSociale}</h3>
+          <p>{company.secteur}</p>
+        </div>
         <img
           className="company-logo"
           src={company.image}
           alt={company.raisonSociale}
         />
-      </div>
-      <div className="company-card-body">
-        <h3>{company.raisonSociale}</h3>
-        <p>{company.secteur}</p>
       </div>
     </div>
   );
